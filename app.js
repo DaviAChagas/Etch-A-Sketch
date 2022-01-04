8910f6b0551a20eed334d1df4ef205b5f9ab0ca1
Davi Chagas <davi.a.chagas@unesp.br>
2022-01-04 10:52:20 -0300

    Now can change the density of pixels and add new colors

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 const gridElement = document.querySelector('#grid')
+const rangeSlider = document.querySelector('#range')
+const display = document.querySelector('.display')
+const colorPicker = document.querySelector('#colorPicker')
 
-gridElement.style.gridTemplateColumns = `${gridElement.clientWidth/16}px `.repeat(16)
-gridElement.style.gridTemplateRows = `${gridElement.clientWidth/16}px `.repeat(16)
+const colorButton = document.querySelector('.colorButton')
+const colorfulButton = document.querySelector('.colorfulButton')
+const eraserButton = document.querySelector('.eraserButton')
+const clearButton = document.querySelector('.clearButton')
 
-    let changeColor = (e) => {
-        e.target.style.background = 'black'
-    }
+let pixelColor = 'rgba(41, 41, 41)'
 
-for(let i = 0; i < (16 * 16); i++) {
-    const div = document.createElement('div')
-
-    gridElement.appendChild(div)
+let resetGridElement = () => {
+    gridElement.textContent = ' '
 }
 
+let changeColor = (e) => {
+    if(e.target.classList.contains('pixel') === true) {
+    e.target.style.background = pixelColor
+}}
 
-const divs = document.querySelectorAll('div')
-
-divs.forEach(pixel => {
-    pixel.addEventListener('mouseover', changeColor)
+colorPicker.addEventListener('input', () => {
+        pixelColor = colorPicker.value
 })
+
+eraserButton.addEventListener('click', () => {
+    pixelColor = '#F1F1F1'
+})
+
+let teste = value => {
+    resetGridElement()
+    gridElement.style.gridTemplateColumns = `repeat(${value}, 1fr)`
+    gridElement.style.gridTemplateRows = `repeat(${value}, 1fr)`
+    
+
+    for(let i = 0; i < (value * value); i++) {
+        const div = document.createElement('div')
+        div.classList.add('pixel')
+        gridElement.appendChild(div)
+    }
+    
+    gridElement.addEventListener('mouseover', event => changeColor(event)) 
+}
+    
+let valueToRangeSlider = 16
+
+teste(valueToRangeSlider)
+
+rangeSlider.addEventListener('mousemove', () => {
+    valueToRangeSlider = rangeSlider.value
+    display.textContent = `${valueToRangeSlider} x ${valueToRangeSlider}`    
+})
+
+rangeSlider.addEventListener('input', () => {
+    valueToRangeSlider = rangeSlider.value
+    display.textContent = `${valueToRangeSlider} x ${valueToRangeSlider}`    
+    teste(valueToRangeSlider)
+})
+
+clearButton.addEventListener('click', teste(16))
